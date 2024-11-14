c6e5bb997e3ff17da2e8779633ce0a47ba3e8dd1
Mārtiņš Barkovs <mbarkovs@gmail.com>
2024-11-14 17:33:40 +0200

    add device list fetch to DevicesList

--- src/app/components/DevicesTable.tsx
+++ src/app/components/DevicesTable.tsx
@@ -1,3 +1,9 @@
-export default function DevicesTable() {
-  return <div>TABLE</div>;
+export default async function DevicesTable() {
+  const result = await fetch(
+    "https://static.ui.com/fingerprint/ui/public.json"
+  ).then((res) => res.json());
+
+  console.log(result);
+
+  return <div>TABLE {JSON.stringify(result, null, 2)}</div>;
 }
