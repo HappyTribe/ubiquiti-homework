651d1b5b3f6da97461eb6f0a44b744dce3612803
Mārtiņš Barkovs <mbarkovs@gmail.com>
2024-11-16 17:05:31 +0200

    add ability to close json details

--- src/app/devices/[deviceId]/components/ShowJSONButton.tsx
+++ src/app/devices/[deviceId]/components/ShowJSONButton.tsx
@@ -6,21 +6,24 @@ import Button from "src/ui/Button";
 export default function ShowJSONButton(props: { json: string }) {
   const [showJSON, setShowJSON] = useState<boolean>(false);
 
-  function onShowJson() {
-    setShowJSON(true);
-  }
-
-  if (showJSON) {
-    return (
-      <div className="flex self-center bg-[#F9FAFA] p-2 text-xs max-h-[400px] overflow-y-auto rounded-lg mt-10">
-        <pre>{props.json}</pre>
-      </div>
-    );
+  function onToggleShowJSON() {
+    setShowJSON((prevState) => !prevState);
   }
 
   return (
-    <Button className="self-center text-primary mt-4" onClick={onShowJson}>
-      See All Details as JSON
-    </Button>
+    <>
+      <Button
+        className="self-center text-primary mt-4"
+        onClick={onToggleShowJSON}
+      >
+        {showJSON ? "Hide JSON Details" : "See All Details as JSON"}
+      </Button>
+
+      {showJSON && (
+        <div className="flex self-center bg-[#F9FAFA] p-2 text-xs max-h-[400px] overflow-y-auto rounded-lg mt-4">
+          <pre>{props.json}</pre>
+        </div>
+      )}
+    </>
   );
 }
