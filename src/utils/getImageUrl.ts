9774e29e4d579cd2174ad32bc1061c65a5d2c582
Mārtiņš Barkovs <mbarkovs@gmail.com>
2024-11-16 12:53:21 +0200

    add getImageUrl util

---
+++ src/utils/getImageUrl.ts
@@ -0,0 +1,9 @@
+export default function getImageUrl(params: {
+  id: string;
+  default: string;
+  size?: number;
+}) {
+  return `https://images.svc.ui.com/?u=https%3A%2F%2Fstatic.ui.com%2Ffingerprint%2Fui%2Fimages%2F${
+    params.id
+  }%2Fdefault%2${params.default}.png&w=${String(params.size ?? 640)}&q=75`;
+}
