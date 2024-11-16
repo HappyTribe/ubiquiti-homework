49219a47b1d33ab9c8f77298a10f05a52aebf940
Mārtiņš Barkovs <mbarkovs@gmail.com>
2024-11-16 17:38:22 +0200

    add icon variant to button comp

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -3,11 +3,13 @@ import clsn from "src/utils/clsn";
 
 export default function Button({
   asChild,
+  variant = "default",
   className,
   children,
   ...rest
 }: React.ButtonHTMLAttributes<HTMLButtonElement> & {
   asChild?: boolean;
+  variant?: "default" | "icon";
 }) {
   const Comp = asChild ? Slot : "button";
 
@@ -15,6 +17,9 @@ export default function Button({
     <Comp
       className={clsn(
         "flex items-center justify-center hover:bg-[#f4f5f6] outline-none focus:ring-1 focus:ring-primary data-[active]:bg-[#f4f5f6] text-[#838691] data-[active]:text-primary rounded-md text-sm px-1 py-0.5",
+        {
+          "border-[1px] border-[#EDEDF0] w-8 h-8": variant === "icon",
+        },
         className
       )}
       {...rest}
